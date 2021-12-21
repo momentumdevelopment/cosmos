26043a1bd5233e9fa99ff039eaa7aa14127e336a
linustouchtips <68214996+linustouchtips@users.noreply.github.com>
2021-12-21 11:23:46 -0800

    [IMPL] Up Version to 1.2.0

--- src/main/java/cope/cosmos/client/Cosmos.java
+++ src/main/java/cope/cosmos/client/Cosmos.java
@@ -35,7 +35,7 @@ public class Cosmos {
     // mod info
     public static final String MOD_ID = "cosmos";
     public static final String NAME = "Cosmos";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     // client event bus
     public static EventBus EVENT_BUS = MinecraftForge.EVENT_BUS;
