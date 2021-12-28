ab5fad56e919950022a34c97ca3238cc3c05ad6b
Doogie13 <jay.finbow13@gmail.com>
2021-12-28 16:00:57 +0000

    Added new stuff to LongJump and fixed jumping issue

--- src/main/java/cope/cosmos/client/features/modules/movement/LongJump.java
+++ src/main/java/cope/cosmos/client/features/modules/movement/LongJump.java
@@ -1,17 +1,23 @@
 package cope.cosmos.client.features.modules.movement;
 
 import cope.cosmos.client.events.MotionEvent;
+import cope.cosmos.client.events.PacketEvent;
 import cope.cosmos.client.features.modules.Category;
 import cope.cosmos.client.features.modules.Module;
 import cope.cosmos.client.features.setting.Setting;
+import cope.cosmos.util.player.MotionUtil;
 import net.minecraft.init.MobEffects;
+import net.minecraft.network.play.server.SPacketPlayerPosLook;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class LongJump extends Module {
 
-    public static Setting<Double> speed = new Setting<>("Speed", 0.2, 0.4, 1d, 2);
+    public static Setting<Double> speed = new Setting<>("Boost", 0d, .6, 1d, 2);
+    public static Setting<Boolean> low = new Setting<>("Low",false);
     public static Setting<Double> accelerate = new Setting<>("Accelerate",0.92,0.92,1.10,2);
-    public static Setting<Double> glide = new Setting<>("Glide", 0.0, 0.5, 1d, 2);
+    public static Setting<Double> glide = new Setting<>("Glide", 0.0, .65, 1d, 2);
+    public static Setting<Boolean> disable = new Setting<>("AutoDisable", true).setDescription("Disable on rubberband");
+
     public LongJump INSTANCE;
     double currentSpeed;
 
@@ -24,9 +30,9 @@ public class LongJump extends Module {
 
         event.setCanceled(true);
 
-        if (mc.player.onGround) {
+        if (mc.player.onGround && MotionUtil.isMoving()) {
             event.setY(.42);
-            mc.player.motionY = .42;
+            mc.player.motionY = low.getValue() ? 0 : .42;
             currentSpeed = speed.getValue();
 
             if (mc.player.isPotionActive(MobEffects.SPEED)) {
@@ -40,37 +46,19 @@ public class LongJump extends Module {
 
         }
 
-        event.setX(dir(currentSpeed)[0]);
-        event.setZ(dir(currentSpeed)[1]);
+        event.setX(MotionUtil.getMoveSpeed(Math.max(currentSpeed + .20855, 0.28055))[0]);
+        event.setZ(MotionUtil.getMoveSpeed(Math.max(currentSpeed + .20855, 0.28055))[1]);
 
         currentSpeed *= accelerate.getValue();
 
     }
 
-    double[] dir(double currentSpeed) {
+    @SubscribeEvent
+    void onPacket(PacketEvent.PacketReceiveEvent event) {
 
-        double forward = mc.player.moveForward;
-        double strafe = mc.player.moveStrafing;
-        double yaw = mc.player.rotationYaw;
+        if (event.getPacket() instanceof SPacketPlayerPosLook && disable.getValue())
+            disable();
 
-        if (strafe > 0) {
-            yaw += forward > 0 ? -45 : 45;
-        } else if (strafe < 0) {
-            yaw += forward > 0 ? 45 : -45;
-        }
-
-        strafe = 0;
-
-        if (forward > 0) {
-            forward = 1;
-        } else if (forward < 0) {
-            forward = -1;
-        }
-
-        double cos = Math.cos(Math.toRadians(yaw + 90));
-        double sin = Math.sin(Math.toRadians(yaw + 90));
-
-        return new double[]{((forward * currentSpeed * cos) + (strafe * currentSpeed * sin)), ((forward * currentSpeed * sin) - (strafe * currentSpeed * cos))};
     }
 
 }
