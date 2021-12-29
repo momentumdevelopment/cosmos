e726c58253c0ce160c9a1cfc732f448bb1f97e28
Doogie13 <jay.finbow13@gmail.com>
2021-12-29 13:53:48 +0000

    Comment code and add descriptions to LongJump

--- src/main/java/cope/cosmos/client/features/modules/movement/LongJump.java
+++ src/main/java/cope/cosmos/client/features/modules/movement/LongJump.java
@@ -10,12 +10,17 @@ import net.minecraft.init.MobEffects;
 import net.minecraft.network.play.server.SPacketPlayerPosLook;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+/**
+ * @author Doogie13
+ * @since 27/12/2021
+ * */
+
 public class LongJump extends Module {
 
-    public static Setting<Double> speed = new Setting<>("Boost", 0d, .6, 1d, 2);
-    public static Setting<Boolean> low = new Setting<>("Low",false);
-    public static Setting<Double> accelerate = new Setting<>("Accelerate",0.92,0.92,1.10,2);
-    public static Setting<Double> glide = new Setting<>("Glide", 0.0, .65, 1d, 2);
+    public static Setting<Double> speed = new Setting<>("Boost", 0d, .6, 1d, 2).setDescription("Initial speed");
+    public static Setting<Boolean> low = new Setting<>("Low",false).setDescription("Do a lower hop");
+    public static Setting<Double> accelerate = new Setting<>("Accelerate",0.92,0.92,1.10,2).setDescription("Increase or decrease speed every tick");
+    public static Setting<Double> glide = new Setting<>("Glide", 0.0, .65, 1d, 2).setDescription("Decrease fall speed");
     public static Setting<Boolean> disable = new Setting<>("AutoDisable", true).setDescription("Disable on rubberband");
 
     public LongJump INSTANCE;
@@ -28,13 +33,21 @@ public class LongJump extends Module {
     @SubscribeEvent
     public void onMotion(MotionEvent event) {
 
+        // Vanilla movement
         event.setCanceled(true);
 
+        // If we are onGround and are actually trying to move
         if (mc.player.onGround && MotionUtil.isMoving()) {
+            // Set the event motion to .42
             event.setY(.42);
+
+            // Only make the motion .42 if we want to do a normal hop
             mc.player.motionY = low.getValue() ? 0 : .42;
+
+            // Reset our current speed
             currentSpeed = speed.getValue();
 
+            // Apply speed effect if present
             if (mc.player.isPotionActive(MobEffects.SPEED)) {
                 int amplifier = mc.player.getActivePotionEffect(MobEffects.SPEED).getAmplifier();
                 currentSpeed *= 1 + (0.2 * (amplifier + 1));
@@ -42,13 +55,16 @@ public class LongJump extends Module {
 
         } else if (mc.player.motionY < 0) {
 
+            // If we are falling, slow our fall
             event.setY(event.getY() * glide.getValue());
 
         }
 
+        // Set motion to the speed (or minimum air speed, whichever is higher)
         event.setX(MotionUtil.getMoveSpeed(Math.max(currentSpeed + .20855, 0.28055))[0]);
         event.setZ(MotionUtil.getMoveSpeed(Math.max(currentSpeed + .20855, 0.28055))[1]);
 
+        // Multiply speed by acceleration (for next tick)
         currentSpeed *= accelerate.getValue();
 
     }
@@ -56,6 +72,7 @@ public class LongJump extends Module {
     @SubscribeEvent
     void onPacket(PacketEvent.PacketReceiveEvent event) {
 
+        // Disable on rubberband or teleport
         if (event.getPacket() instanceof SPacketPlayerPosLook && disable.getValue())
             disable();
 
