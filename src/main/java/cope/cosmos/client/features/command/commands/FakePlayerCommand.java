89243e2cbf8834d840cbf6a4a4b41a558de2f272
Sxmurai <gavrw0607@gmail.com>
2022-04-10 13:52:26 -0500

    [IMPL] Fakeplayer command

---
+++ src/main/java/cope/cosmos/client/features/command/commands/FakePlayerCommand.java
@@ -0,0 +1,20 @@
+package cope.cosmos.client.features.command.commands;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import cope.cosmos.client.features.command.Command;
+import cope.cosmos.client.features.modules.misc.FakePlayerModule;
+
+/**
+ * Placeholder, going to be added onto
+ *
+ * @author aesthetical
+ */
+public class FakePlayerCommand extends Command {
+    public FakePlayerCommand() {
+        super("fakeplayer", "Manages the fakeplayer", LiteralArgumentBuilder.literal("fakeplayer")
+                .executes((ctx) -> {
+                    FakePlayerModule.INSTANCE.toggle();;
+                    return 0;
+                }));
+    }
+}
